112f2aa9c1834b2f98f074bdf94288c93fd5cba6
Sam Harrison <Sam.Harrison@autotrader.co.uk>
2023-01-05 16:00:14 +0000

    feat: sorted music and instruction modal click

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
+// Get Elements
+
+const dropdown = document.getElementById("instructions-modal");
+const imageContainer = document.getElementsByClassName("image-container");
+const volumeOn = document.getElementById("volume-off");
+const volumeOff = document.getElementById("volume-on");
+const myAudio = document.querySelector("#audio");
+
 // Toggle Modal
 
-const handleModalClicked = (event) => {
-    event.stopPropagation();
-    const dropdown = document.getElementById("instructions-modal");
-    toggleDropdown(!dropdown.classList.contains("open"));
-  };
+function handleModalClicked(event) {
+  event.stopPropagation();
+  toggleDropdown(!dropdown.classList.contains("open"));
+}
 
-const toggleDropdown = (shouldOpen) => {
-    const dropdown = document.getElementById("instructions-modal");
-  
-    if (shouldOpen) {
-      dropdown.classList.add("open");
-    } else {
-      dropdown.classList.remove("open");
-    }
-  
-  };
-  
-document.body.addEventListener("click", () => toggleDropdown());
+function toggleDropdown(shouldOpen) {
+  if (shouldOpen) {
+    dropdown.classList.add("open");
+  } else {
+    dropdown.classList.remove("open");
+  }
+}
+
+dropdown.addEventListener("click", () => toggleDropdown());
 
 // Toggle Music
 
-const handleVolume = (event) => {
+function handleVolume(event) {
   event.stopPropagation();
-  const volumeOn = document.getElementById("volume-off");
   toggleVolume(!volumeOn.classList.contains("on"));
-};
+}
 
-const toggleVolume = (shouldBeOn) => {
-  const volumeOn = document.getElementById("volume-off");
-  const volumeOff = document.getElementById("volume-on");
-  const myAudio = document.querySelector('#audio')
-
+function toggleVolume(shouldBeOn) {
   if (shouldBeOn) {
     volumeOn.classList.add("on");
-    volumeOff.classList.add('show');
-
+    volumeOff.classList.add("show");
     myAudio.play();
+    myAudio.addEventListener("timeupdate", function () {
+      let buffer = 0.44;
 
-    myAudio.addEventListener('timeupdate', function(){
-      var buffer = .44
-      if(this.currentTime > this.duration - buffer){
-          this.currentTime = 0
-          this.play()
+      if (this.currentTime > this.duration - buffer) {
+        this.currentTime = 0;
+        this.play();
       }
     });
-
   } else {
     volumeOn.classList.remove("on");
-    myAudio.pause()
+    myAudio.pause();
   }
+}
 
-};
-
-document.body.addEventListener("click", () => toggleVolume());
+imageContainer.addEventListener("click", () => toggleVolume());
